1c40f8b44f4fd97f1808c6c8f9dadba08119d9b9
yilmaz <ydaskafa@gmail.com>
2021-03-13 22:51:29 +0300

    created sign up view and little style operations, user create with signup form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -36,18 +37,30 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
+    /**
+     * @Route("/signupview", name="app_signup_view")
+     */
+    public function signupView(): Response{
+        return $this->render('security/signup.html.twig');
+    }
+
+
+
 
     /**
      * @Route("/signup", name="app_signup")
      */
-    public function signup(UserPasswordEncoderInterface $encoder): Response{
+    public function signup(UserPasswordEncoderInterface $encoder, Request $request): Response{
 
         $user = new User();
+//        dd($request->get('email'));
 
-        $encoded = $encoder->encodePassword($user, '123456');
+        $email = $request->get('email');
+        $encodedPassword = $encoder->encodePassword($user, $request->get('password'));
 
-        $user->setEmail('volkan.sengul@test.com')
-            ->setPassword($encoded)
+        $user->setEmail($email)
+            ->setPassword($encodedPassword)
+            ->setIsActive('')
             ->setRoles(['ROLE_USER']);
 
         $em = $this->getDoctrine()->getManager();
