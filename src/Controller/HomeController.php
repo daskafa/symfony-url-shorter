ee25ef51abb337f1d3484b0cbfe1ae898762b65f
yilmaz <ydaskafa@gmail.com>
2021-03-15 20:31:26 +0300

    included tailwind and start new interface

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends AbstractController
         $posts = $homepageInterfaceRepository->findAll();
 
 
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/test.html.twig', [
             'posts' => $posts
         ]);
     }
